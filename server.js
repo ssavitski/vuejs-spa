a43bb2ab122aa27b77d1ff43e38434a4676e223d
Siarhei Savitski <s.savitski@myxtramile.com>
2018-07-16 15:22:26 +0300

    Add express server for launching application

---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const fs = require('fs');
+const path = require('path');
+
+const indexHTML = (() => {
+  return fs.readFileSync(path.resolve(__dirname, './index.html'), 'utf-8');
+})();
+
+app.get('*', (req, res) => {
+  res.write(indexHTML);
+  res.end();
+});
+
+const port = process.env.port || 3500;
+app.listen(port, () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
