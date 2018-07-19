503271193fd81ea4f8945b4bb75564e9f0da9049
Siarhei Savitski <s.savitski@myxtramile.com>
2018-07-19 12:10:58 +0300

    Add navigation and login component

--- src/router.js
+++ src/router.js
@@ -2,14 +2,18 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 
 import Category from './theme/Category.vue';
+import Login from './theme/Login.vue';
 
 Vue.use(VueRouter);
 
 const router = new VueRouter({
+  mode: 'history',
+  linkActiveClass: 'is-active',
+  scrollBehavior: () => ({ y: 0 }),
   routes: [
-    {
-      path: '/', component: Category,
-    },
+    { path: '/category/front-end', component: Category },
+    { path: '/login', component: Login },
+    { path: '/', redirect: '/category/front-end' },
   ],
 });
 
--- src/theme/AppHeader.vue
+++ src/theme/AppHeader.vue
@@ -1,10 +1,21 @@
 <template>
   <nav class="nav has-shadow">
     <div class="container">
-      <a href="/">
+      <router-link to="/" exact>
         <img src="./../img/logo.png"
              alt="Vue SPA" />
-      </a>
+      </router-link>
+
+      <router-link to="/"
+                   exact
+                   class="nav-item is-tab">
+        Home
+      </router-link>
+
+      <router-link to="/login"
+                   class="nav-item is-tab">
+        Login
+      </router-link>
     </div>
   </nav>
 </template>
