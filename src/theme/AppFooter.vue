dfa748c3c52e4ec2aed2a93c56d1a29186430a11
Siarhei Savitski <s.savitski@myxtramile.com>
2018-07-19 10:15:09 +0300

    Breakdown to the components

---
+++ src/theme/AppFooter.vue
@@ -0,0 +1,11 @@
+<template>
+  <footer class="footer">
+    <div class="container">
+      <div class="content has-text-centered">
+        Follow us on
+        <a href="https://www.facebook.com/siarhei.savitski"
+           target="_blank">Facebook</a>
+      </div>
+    </div>
+  </footer>
+</template>
---
+++ src/theme/AppHeader.vue
@@ -0,0 +1,10 @@
+<template>
+  <nav class="nav has-shadow">
+    <div class="container">
+      <a href="/">
+        <img src="./../img/logo.png"
+             alt="Vue SPA" />
+      </a>
+    </div>
+  </nav>
+</template>
