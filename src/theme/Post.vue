6a867ce748581f5b34c587354cdddfd271a4c3e1
Siarhei Savitski <s.savitski@myxtramile.com>
2018-07-19 11:01:44 +0300

    Extract styles into separate file

--- src/theme/Post.vue
+++ src/theme/Post.vue
@@ -22,3 +22,18 @@
     ],
   };
 </script>
+
+<style lang="scss" scoped>
+  .card {
+    padding-bottom: 40px;
+    height: 100%;
+  }
+
+  footer {
+    position: absolute;
+    bottom: 0;
+    width: 100%;
+    left: 0;
+  }
+</style>
+
